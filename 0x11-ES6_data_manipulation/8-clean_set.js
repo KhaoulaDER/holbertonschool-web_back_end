afab2f9cae9da034ad684863d6295728bc99eccd
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2022-04-20 12:35:11 +0100

    Update 8-clean_set.js

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -3,12 +3,11 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
     throw new TypeError('set must be of type Set');
   }
+ 
   let returnStr = '';
-  if (startString.length === 0) {
+  
+  if (typeof (startString) !== 'string' || startString.length === 0) {
     return returnStr;
-  }
-  if (typeof (startString) !== 'string') {
-    throw new TypeError('startString must be of type String');
   }
   for (const str of set) {
     if (typeof (str) !== 'undefined'){
